8c2b61941322593c0561c9e3f8f307a8128a1f50
Sergej <62400556+serdjukow@users.noreply.github.com>
2021-03-21 07:51:13 +0100

    Creat-Menu-JS

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+// // Import jQuery module (npm i jquery)
+// import $ from 'jquery'
+// window.jQuery = $
+// window.$ = $
+
+// // Import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	var CurrentYear = new Date().getFullYear();
+	document.getElementById('currentDate').innerHTML = CurrentYear
+
+})
